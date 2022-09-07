ca4a81430c94091b40b9f3c9840050e4b6591f24
Doridian <git@doridian.net>
2022-09-06 20:06:22 -0700

    and it works, finally

--- agent/main.go
+++ agent/main.go
@@ -13,10 +13,18 @@ import (
 //go:embed test.png
 var imageData []byte
 
+//go:embed test2.png
+var image2Data []byte
+
 func main() {
 	img, err := png.Decode(bytes.NewReader(imageData))
 	if err != nil {
 		log.Panicf("No embdedded image: %v", err)
+	}
+
+	img2, err := png.Decode(bytes.NewReader(image2Data))
+	if err != nil {
+		log.Panicf("No embdedded image2: %v", err)
 	}
 
 	devs, err := streamdeck.Devices()
@@ -33,4 +41,20 @@ func main() {
 	defer d.Close()
 
 	d.SetImage(0, img)
+	d.SetImage(1, img2)
+
+	buttonChan, err := d.ReadKeys()
+	if err != nil {
+		log.Panicf("can't read keys: %v", err)
+	}
+
+	for {
+		b := <-buttonChan
+		log.Printf("B: %v, P: %v", b.Index, b.Pressed)
+		if b.Pressed {
+			d.SetImage(b.Index, img)
+		} else {
+			d.SetImage(b.Index, img2)
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -2,11 +2,11 @@ module github.com/Doridian/streamdeckpi
 
 go 1.19
 
-require github.com/Doridian/streamdeck v0.0.0-20220907015624-9bf18f1d511d
+require github.com/Doridian/streamdeck v0.1.0
 
 require (
-	github.com/jkassis/hid v0.0.0-20220630003547-398145ff2de0 // indirect
-	golang.org/x/image v0.0.0-20200119044424-58c23975cae1 // indirect
+	github.com/KarpelesLab/hid v0.1.0 // indirect
+	golang.org/x/image v0.0.0-20220902085622-e7cb96979f69 // indirect
 )
 
 // Well this seems like a weird kludge, but it works to make gokrazy understand to use the local files...
