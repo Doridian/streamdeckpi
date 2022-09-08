9cd2bc636890de2e74afebf716a656f3ca1d15b3
Doridian <git@doridian.net>
2022-09-07 20:30:46 -0700

    make none a valid action

--- agent/actions/none.go
+++ agent/actions/none.go
@@ -1,5 +1,11 @@
 package actions
 
+import "github.com/Doridian/streamdeckpi/agent/interfaces"
+
 type None struct {
 	actionWithIcon
+}
+
+func (a *None) Run(pressed bool, controller interfaces.Controller) error {
+	return nil
 }
