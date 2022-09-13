444e704d67513b8883fabc4e54541cc69685a0be
Doridian <git@doridian.net>
2022-09-13 08:54:47 -0700

    and make the real package name

--- build.sh
+++ build.sh
@@ -2,16 +2,13 @@
 export GOARCH=arm
 export GOARM=6
 
-#  -update=yes \
-
+#  -overwrite /dev/disk4 \
 
 ~/go/bin/gokr-packer \
   -tls=self-signed \
   -kernel_package=github.com/gokrazy-community/kernel-rpi-os-32/dist \
   -hostname streamdeckpi \
   -serial_console serial0,115200 \
-  -overwrite /dev/disk4 \
-  github.com/gokrazy/breakglass \
-  github.com/gokrazy/serial-busybox \
+  -update=yes \
   github.com/gokrazy/wifi \
-  github.com/Doridian/streamdeckpi/agent
+  github.com/Doridian/streamdeckpi/agent/cmd/agent
