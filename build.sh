66b56ad910834ac2ae20d513c40e0177bf1cd35b
Doridian <git@doridian.net>
2022-09-13 19:17:13 -0700

    allow arg for overwrite

--- build.sh
+++ build.sh
@@ -2,13 +2,18 @@
 export GOARCH=arm
 export GOARM=6
 
-#  -overwrite /dev/disk4 \
+UPARG='-update=yes'
+if [ ! -z "$1" ]
+then
+  diskutil unmountDisk "$1"
+  UPARG="-overwrite=$1"
+fi
 
 ~/go/bin/gokr-packer \
   -tls=self-signed \
   -kernel_package=github.com/gokrazy-community/kernel-rpi-os-32/dist \
-  -hostname streamdeckpi \
-  -serial_console serial0,115200 \
-  -update=yes \
+  -hostname=streamdeckpi \
+  -serial_console=serial0,115200 \
+  "$UPARG" \
   github.com/gokrazy/wifi \
   github.com/Doridian/streamdeckpi/agent/cmd/agent
