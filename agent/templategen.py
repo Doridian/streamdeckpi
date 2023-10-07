814705cd68bcf4f070abdce77bacf84d59ac6130
Doridian <git@doridian.net>
2023-10-06 22:30:16 -0700

    colored gauges

--- agent/templategen.py
+++ agent/templategen.py
@@ -57,43 +57,36 @@ def make_fancy_light(entity_id: str, icon_type: str, pos: list[int]):
         },
     }
 
-PAGES = {}
-
-def make_default_page():
-    actions = []
-    actions.append(make_fancy_light("light.hue_color_lamp_1_3", "floor_light_top", [0, 0]))
-    actions.append(make_fancy_light("light.hue_color_candle_1_2", "floor_light_bottom", [0, 1]))
-    actions.append(make_fancy_light("light.hue_lightguide_bulb_1", "ceiling_light", [1, 0]))
-
-    actions.append({
-        "button": [2, 0],
+def make_gauge(gaugetype: str, entity_domain: str, entity_id: str, thresholds: list[float], title: str, pos: list[int]):
+    return {
+        "button": pos,
         "name": "homeassistant_string",
         "parameters": {
-            "domain": "sensor",
-            "entity": "sensor.dori_office_co2",
-            "icon": "icons/gauge_full.png",
+            "domain": entity_domain,
+            "entity": entity_id,
+            "icon": f"icons/gauge_{gaugetype}_full.png",
             "conditions": [
                 {
                     "condition": {
-                        "comparison": "<",
-                        "value": 600,
+                        "comparison": "<=",
+                        "value": thresholds[0],
                     },
-                    "icon": "icons/gauge_empty.png",
+                    "icon": f"icons/gauge_{gaugetype}_empty.png",
                 },
                 {
                     "condition": {
-                        "comparison": "<",
-                        "value": 1000,
+                        "comparison": "<=",
+                        "value": thresholds[1],
                     },
-                    "icon": "icons/gauge_low.png",
+                    "icon": f"icons/gauge_{gaugetype}_low.png",
                 },
                 {
                     "condition": {
-                        "comparison": "<",
-                        "value": 1500,
+                        "comparison": "<=",
+                        "value": thresholds[2],
                     },
-                    "icon": "icons/gauge_high.png",
-                }
+                    "icon": f"icons/gauge_{gaugetype}_high.png",
+                },
             ],
             "texts": [
                 {
@@ -104,7 +97,7 @@ def make_default_page():
                     "y": 0,
                     "align": "center",
                     "vertical-align": "top",
-                    "text": "CO2",
+                    "text": title,
                 },
                 {
                     "color": [255, 255, 255, 255],
@@ -118,7 +111,22 @@ def make_default_page():
                 }
             ],
         }
-    })
+    }
+    
+
+
+PAGES = {}
+
+def make_default_page():
+    actions = []
+    actions.append(make_fancy_light("light.hue_color_lamp_1_3", "floor_light_top", [0, 0]))
+    actions.append(make_fancy_light("light.hue_color_candle_1_2", "floor_light_bottom", [0, 1]))
+    actions.append(make_fancy_light("light.hue_lightguide_bulb_1", "ceiling_light", [1, 0]))
+
+    actions.append(make_gauge("lowcolor", "sensor", "sensor.dori_office_co2", [600, 1000, 1500], "CO2", [2, 0]))
+    actions.append(make_gauge("lowcolor", "sensor", "sensor.dori_office_particulate_matter_1_0um_concentration", [10, 50, 500], "1.0 um", [3, 0]))
+    actions.append(make_gauge("lowcolor", "sensor", "sensor.dori_office_particulate_matter_2_5um_concentration", [10, 50, 500], "2.5 um", [4, 0]))
+    actions.append(make_gauge("lowcolor", "sensor", "sensor.dori_office_particulate_matter_10_0um_concentration", [10, 50, 500], "10 um", [5, 0]))
 
     PAGES["default"] = {"actions":actions}
 
