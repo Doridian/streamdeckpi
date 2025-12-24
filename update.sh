14c7e5c06952ba7f3747187675de59bd37352bc2
Doridian <git@doridian.net>
2025-12-24 10:04:32 -0800

    Delete old stuff

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -ex
 go install github.com/gokrazy/tools/cmd/gok@main
+cd agent && rm -fv _gokrazy/extrafiles/etc/streamdeckpi/*.yml && ./templategen.py && cd ..
 rm -rf builddir
 ~/go/bin/gok update --instance streamdeckpi
